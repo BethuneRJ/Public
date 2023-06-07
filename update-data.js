6f731f9578f8be56212c009e257abefa6d736b88
BethuneRJ <121315765+BethuneRJ@users.noreply.github.com>
2023-06-07 08:33:48 -0400

    Create update-data.js

---
+++ update-data.js
@@ -0,0 +1,14 @@
+// update-data.js
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+const DATA_URL = 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Active_Hurricanes_v1/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=geojson';
+const FILE_PATH = 'ArcGIS-Hurricane-Data.json';
+
+fetch(DATA_URL)
+  .then(response => response.json())
+  .then(data => {
+    fs.writeFileSync(FILE_PATH, JSON.stringify(data, null, 2));
+    console.log('Data updated successfully');
+  })
+  .catch(error => console.error('Error:', error));
